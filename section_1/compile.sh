29f122f36ed315f18ed4beb23e29493b5a5b361d
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-02 22:18:56 +0100

    Adding compilation script for faster testing

---
+++ section_1/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Compile the source code
+echo "Compiling the source code..."
+g++ -o executable $1
+echo "Compilation complete."
+echo "Running the program..."
+./executable
+echo "Program complete."
+rm executable
+
