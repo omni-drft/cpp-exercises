e4ebc01a9cff96a9d68f58ac03db7cbb509fd163
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-09 22:58:10 +0100

    Adding 2.2 solution and improving 2.1

--- section_2/201.cpp
+++ section_2/201.cpp
@@ -4,9 +4,8 @@ int main() {
 
     std::string name {"Piotr"};    
     for (char i : name) {
-        if( i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'y') {
+        if( i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'y')
             std::cout << name << std::endl;
-        }
     }
 
     return 0;
---
+++ section_2/202.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+int high(std::vector<int>& vec);
+
+int main() {
+
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(1, 10);
+
+    std::vector<int> vec {};
+    vec.reserve(3);
+
+    for (int i = 0; i < 3; i++)
+        vec.push_back(distribution(generator));
+    
+    std::cout << std::endl;
+    
+    int max {high(vec)};
+    int sum {0};
+    for (int i : vec)
+        sum += i;
+    for(int i {0}; i < max; i++)
+        std::cout << sum << std::endl;
+
+    return 0;
+}
+
+int high(std::vector<int>& vec) {
+    int max = vec[0];
+    int index = 0;
+    for (int i {1}; i < vec.size(); i++) {
+        if (vec[i] > max) {
+            max = vec[i];
+            index = i;
+        }
+    }
+    vec.erase(vec.begin() + index);
+    return max;
+}
