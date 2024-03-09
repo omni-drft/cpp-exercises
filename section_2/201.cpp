59308ef990f6f3407c3530f8567d3f6d1c32f36b
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-09 22:41:09 +0100

    Adding 2.1 solution

--- section_2/201.cpp
+++ section_2/201.cpp
@@ -2,7 +2,12 @@
 
 int main() {
 
-    
+    std::string name {"Piotr"};    
+    for (char i : name) {
+        if( i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' || i == 'y') {
+            std::cout << name << std::endl;
+        }
+    }
 
     return 0;
 }
