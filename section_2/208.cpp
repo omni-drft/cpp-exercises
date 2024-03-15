3182a6b13e12bea846467ef7ece7847594ed8178
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-15 20:19:55 +0100

    Adding solution 2.8

---
+++ section_2/208.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+int main() {
+
+    srand(time(NULL));
+    
+    double result {};
+    int first = rand() % 10;
+    
+    if(first == 9) {
+        result = 1;
+        std::cout << std::fixed << std::setprecision(3) << result << std::endl;
+        return 0;
+    }
+
+    result = 0;
+
+    result += (rand() % 10) * 0.1;
+    result += (rand() % 10) * 0.01;
+    result += (rand() % 10) * 0.001;
+
+    std::cout << std::fixed << std::setprecision(3) << result << std::endl;
+
+    return 0;
+}
