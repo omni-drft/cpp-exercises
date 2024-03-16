655feeb8b6c3e65b04bef332e722334eb21c5682
Piotr Walczak <piotrwalczak.pv@gmail.com>
2024-03-16 21:29:08 +0100

    Adding 2.11 solution

---
+++ section_2/211.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    
+    int n;
+    std::cin >> n;
+    int z = n & 1;
+
+    if (z == 0)
+        std::cout << "even" << std::endl;
+    else
+        std::cout << "odd" << std::endl;
+
+    return 0;
+}
+
+// 00110011
+// &
+// 00000001
+// =
+// 00000001
+// odd
+
+
+// 00110010
+// &
+// 00000001
+// =
+// 00000000
+// even
